e2a7e6a93f6468eb736cfa78c3cf90e20057c51e
Leonardo Galante <lndgalante@gmail.com>
2020-10-28 16:06:17 -0300

    👌 IMPROVE: Concepts script

--- typeofnan-javascript-quizzes/2-a-set-of-objects.js
+++ typeofnan-javascript-quizzes/2-a-set-of-objects.js
@@ -4,7 +4,8 @@ Challenge:
   A Set of objects
 
 Descripción:
-  Considerando el siguiente Set de objetos propagados en un nuevo array. Qué se loguea?
+  Considerando el siguiente Set de objetos propagados en un nuevo array.
+  Qué se loguea por pantalla?
 
 Posibles opciones:
   - [{ a: 1 }, { a: 1 }]
@@ -14,10 +15,20 @@ Posibles opciones:
 
 Conceptos a entender:
   - Tipos de datos primitivos (7)
-  - Estructura de datos: Set
-  - Operador spread (propagador)
+
+  - Set + Ejemplo de abecedario
+    - Qué es un Set?
+    - Como agregar valores al mismo? (2 maneras)
+    - Como iterar sobre un Set?
+    - Como conocer la cantidad de elementos?
+
+  - Operador spread (propagador) + Ejemplo abecedario
+    - Cómo se utiliza?
+    - Para qué nos sirve?
+    - Qué pasa cuando hacemos un spread de un Set en un array?
+
 */
 
 const mySet = new Set([{ a: 1 }, { a: 1 }])
 const result = [...mySet]
-console.log(result)
+console.log('result', result)
