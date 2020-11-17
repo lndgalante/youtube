469a9f06083ddfd2931880b629b72b70532a068b
Leonardo Galante <lndgalante@gmail.com>
2020-11-17 00:31:42 -0300

    📦 NEW: Add challenge solution

--- typeofnan-javascript-quizzes/3-deep-object-mutability.js
+++ typeofnan-javascript-quizzes/3-deep-object-mutability.js
@@ -56,3 +56,77 @@ Object.freeze(user);
 user.pet.name = 'Daffodil';
 
 console.log('\n: user pet name', user.pet.name);
+
+/*
+
+ 1. Repaso breve de objetos
+  Un objeto como una colección no ordenada de pares clave/valor o key/value
+
+*/
+
+let pokemon = {
+  name: 'Pikachu',
+  level: 2,
+  metadata: {
+    firstRelease: 1996,
+  },
+};
+
+/*
+  2. Property Descriptors
+      Son las propiedades de las keys de un objeto, y por defecto las mismas se inicializan en true, exceptuan el property descriptor value
+
+    - value
+      Nos permite asignar o modificar el valor de una key
+
+    - enumerable (por defecto en true)
+      Nos permite que la key sea iterable o no.
+      Si ejecutamos un loop (for-in) vamos a poder visualizar o no la misma key.
+
+    - writable (por defecto en true)
+      Nos permite que la key se pueda escribir o no, si es false la key sera de solo lectura
+
+    - configurable (por defecto en true)
+      No podremos modificar ninguna de las anteriores si configurable esta seteado en false.
+      Una vez seteado en false, no podremos volver el mismo en true
+
+  3. Como leer las propiedades de una key usando Object.getOwnPropertyDescriptor(obj, key);
+
+  4. Como definir las propiedades de una key usando Object.defineProperty(obj, key, { ...propiedades });
+*/
+
+const pokemonNamePropertyDescriptors = Object.getOwnPropertyDescriptor(pokemon, 'name');
+
+Object.defineProperty(pokemon, 'level', {
+  writable: false,
+  enumerable: false,
+  configurable: false,
+});
+
+pokemon.level = 4;
+
+// Dispara error ya que no podemos modificar nuevamente los property descriptors
+// Object.defineProperty(pokemon, 'level', {
+//   writable: true,
+// });
+
+// const pokemonLevelPropertyDescriptors = Object.getOwnPropertyDescriptor(pokemon, 'level');
+
+// for (const key in pokemon) {
+//   console.log('\n: key', key);
+//   const value = pokemon[key];
+//   console.log('\n: value', value);
+// }
+
+/*
+  5. Que hace Object.freeze(obj) ? Qué propiedades de las keys modifica?
+    El mismo setea writable y configurable en false, de todas las keys de primer nivel del objeto
+*/
+
+Object.freeze(pokemon);
+
+const pokemonMetadataPropertyDescriptors = Object.getOwnPropertyDescriptor(pokemon, 'metadata');
+// console.log('\n: pokemonMetadataPropertyDescriptors', pokemonMetadataPropertyDescriptors);
+
+const pokemonFirstReleasePropertyDescriptors = Object.getOwnPropertyDescriptor(pokemon.metadata, 'firstRelease');
+// console.log('\n: pokemonFirstReleasePropertyDescriptors', pokemonFirstReleasePropertyDescriptors);
