2983a1e6409ce238f77ff685a4bf3502e4b78345
Leonardo Galante <lndgalante@gmail.com>
2020-11-30 01:43:08 -0300

    👌 IMPROVE: Add links

--- typeofnan-javascript-quizzes/4-prototypal-inheritance.js
+++ typeofnan-javascript-quizzes/4-prototypal-inheritance.js
@@ -43,7 +43,8 @@ Conceptos a entender:
 ------------------------------------------------------------
 
 Links:
-  - https://quiz.typeofnan.dev/prototypal-inheritance/
+  - https://javascript.info/prototype-inheritance
+  - https://developer.mozilla.org/en-US/docs/Web/JavaScript/Inheritance_and_the_prototype_chain
 
 */
 
