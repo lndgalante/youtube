3d4bcc5dc53be52a8bcea982181983308e2820f1
Leonardo Galante <lndgalante@gmail.com>
2020-12-01 20:25:10 -0300

    📦 NEW: Initial log content

---
+++ typeofnan-javascript-quizzes/5-console-log-fetch.js
@@ -0,0 +1,37 @@
+/*
+
+Challenge:
+  Console log fetch
+
+Descripción:
+  Qué se loguea cuando tratamos de loguear fetch?
+
+Posibles opciones:
+  - La función fetch
+  - Un error de referencia
+  - Depende según el ambiente que estemos
+
+------------------------------------------------------------
+
+Conceptos a entender:
+  1. Tipos de ambientes
+
+  2. APIs de JavaScript, del Navegador y de Node.js
+
+  3. Creemos una función para conocer en qué ambiente nos encontramos
+
+  4. Solución
+
+------------------------------------------------------------
+
+Extra:
+  - Feature request de fetch en Node.js
+    https://github.com/nodejs/node/issues/19393
+
+  - Next.js con built-in fetch
+    https://nextjs.org/docs/basic-features/supported-browsers-features#server-side-polyfills
+
+Links:
+  - https://developer.mozilla.org/en-US/docs/Web/API/Fetch_API
+
+*/
