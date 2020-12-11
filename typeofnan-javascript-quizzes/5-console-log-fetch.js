10517c9622c9748a54ff45623eb95f510d10d679
leo <lndgalante@gmail.com>
2020-12-11 00:45:08 -0300

    👌 IMPROVE: Log fetch content

--- typeofnan-javascript-quizzes/5-console-log-fetch.js
+++ typeofnan-javascript-quizzes/5-console-log-fetch.js
@@ -14,9 +14,9 @@ Posibles opciones:
 ------------------------------------------------------------
 
 Conceptos a entender:
-  1. Qué son los ambientes? Tipos de ambientes
+  1. Qué son los engines?
 
-  2. Qué son los engines?
+  2. Qué son los ambientes? Tipos de ambientes
 
   3. Diagrama de APIs de JavaScript, del Navegador, de Node.js y Deno
 
@@ -42,19 +42,25 @@ Links:
 
 /*
 
-1. Qué son los ambientes? Tipos de ambientes
-    Son aquellos lugares donde nuestro usuario interactua con la aplicación.
+
+1. Qué son los engines?
+    Un engine es un programa responsable de convertir nuestro código fuente a código máquina o bytecode
+    y posteriormente ejectuar este último en la CPU.
+    Todos los engines implementan las definiciones del lenguaje que son provistas por ECMAScript.
+
+    Siendo el engine más conocido "V8" que son utilizados en Google Chrome, Edge, Brave, Node.js y Deno,
+    SpiderMonkey en Firefox o JavaScriptCore o Nitro en Safari.
+
+2. Qué son los ambientes? Tipos de ambientes
+    En el desarrollo web no usamos los engines directamente sino que estos viven dentro de un ambiente,
+    que además de las APIs de JavaScript que son provistas por el engine, cada ambiente trae consigo
+    su set particular de APIs que nos permiten conectar el mundo de alrededor con el engine.
+    Algunos ejemplos de funcionalidades son el acceso a Bluetooth o el acceso sistema de archivos.
 
     Actualmente a grandes rasgos tenemos 3 ambientes:
     - Los navegadores (Chrome, Firefox, Safari siendo los más conocidos)
     - Node.js
     - Deno
-
-2. Qué son los engines?
-    Estas viven en todos los ambientes ya que la implementación de cada API vive en el engine o motor
-    que necesita correr cada uno de los ambientes para poder además ejecutar el código JavaScript.
-    Siendo el engine más conocido "V8" que son utilizados en Google Chrome, Edge, Brave, Node.js y Deno,
-    SpiderMonkey en Firefox o JavaScriptCore en Safari.
 
 3. Diagrama de APIs de JavaScript, del Navegador, de Node.js y Deno
 
