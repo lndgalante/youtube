b39a0c56347ad0cb840afbf13beeafd065865449
leo <lndgalante@gmail.com>
2020-12-30 18:45:38 -0300

    👌 IMPROVE: Reduce content

--- typeofnan-javascript-quizzes/7-reduce-math.js
+++ typeofnan-javascript-quizzes/7-reduce-math.js
@@ -15,28 +15,89 @@ Posibles opciones:
 ------------------------------------------------------------
 
 Conceptos a entender:
-  1. Reduce
-    a. Ejemplo de suma
-    b. Escribiendo nuestro propio reduce
-    c. Visualización de reduce
+  1. Arrow functions
 
-  2. Solución
+  2. Reduce
+    a. Escribiendo nuestro propio reduce
+    b. Ejemplo de suma de números
+    c. Visualización de reduce, https://reduce.surge.sh
+    d. Inmutabilidad, https://doesitmutate.xyz
+
+  3. Solución
 
 ------------------------------------------------------------
 
 Challenge + Solución:
   - https://quiz.typeofnan.dev/reduce-math
+  - https://github.com/lndgalante/youtube/blob/master/typeofnan-javascript-quizzes/7-reduce-math.js
 
 Links Principales:
+  - https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Functions/Arrow_functions
   - https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/Reduce
 
 Links Secundarios:
-  - https://doesitmutate.xyz
+  - https://javascript.info/arrow-functions-basics
+  - https://javascript.info/array-methods#reduce-reduceright
 
 */
 
-const arr = [(x) => x * 1, (x) => x * 2, (x) => x * 3, (x) => x * 4];
+const array = [(x) => x * 1, (x) => x * 2, (x) => x * 3, (x) => x * 4];
 
-const result = arr.reduce((agg, el) => agg + el(agg), 1);
+const result = array.reduce((accumulator, fn) => accumulator + fn(accumulator), 1);
 
-console.log('The result is:', result);
+// console.log('The result is:', result);
+
+/*
+  1. Arrow functions
+      Las arrow functions o funciones flechas fueron introducidas en ES6 o ES2015.
+      Las mismas traen varios beneficios como:
+        - Return implícito
+        - Sintáxis concisa
+        - This heredado por el contexto más cercano
+*/
+
+function suma(x, y) {
+  return x + y;
+}
+
+/*
+  2. Reduce
+    a. Escribiendo nuestro propio reduce
+*/
+
+function reduce(array, reducer, initialValue) {
+  let accumulator = initialValue;
+
+  for (const element of array) {
+    accumulator = reducer(accumulator, element);
+  }
+
+  return accumulator;
+}
+
+/*
+  2. Reduce
+    b. Ejemplo de suma de números
+*/
+
+const numbers = [3, 5, 1, 4, 2];
+
+const sumReducer = (accumulator, number) => accumulator + number;
+
+const sumOfNumbersCustom = reduce(numbers, sumReducer, 0);
+
+const sumOfNumbers = numbers.reduce((accumulator, number) => accumulator + number, 0);
+
+/*
+  2. Reduce
+    c. Visualización de reduce, https://reduce.surge.sh
+*/
+
+/*
+  2. Reduce
+    d. Inmutabilidad, https://doesitmutate.xyz
+*/
+
+/*
+  3. Solución
+*/
