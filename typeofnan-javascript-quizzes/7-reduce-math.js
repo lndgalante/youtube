dbcd1aad14c51bcc8715484aa01a0a96432056b5
leo <lndgalante@gmail.com>
2021-01-03 13:50:15 -0300

    👌 IMPROVE: Reduce content

--- typeofnan-javascript-quizzes/7-reduce-math.js
+++ typeofnan-javascript-quizzes/7-reduce-math.js
@@ -47,10 +47,13 @@ Links Secundarios:
 /*
   1. Arrow functions
       Las arrow functions o funciones flechas fueron introducidas en ES6 o ES2015.
+
       Las mismas traen varios beneficios como:
         - Return implícito
         - Sintáxis concisa
         - This heredado por el contexto más cercano
+
+      Se recomienda utilizar las arrow functions en los callbacks y no definir todas las funciones como arrow funcitons.
 */
 
 function sum(x, y) {
@@ -60,45 +63,3 @@ function sum(x, y) {
 function randomNumber() {
   return Math.random();
 }
-
-/*
-  2. Reduce
-    a. Definición y visualización de reduce, https://reduce.surge.sh
-*/
-
-//  El método reduce se suele utilizar cuando queremos calcular un valor único.
-
-/*
-  2. Reduce
-    b. Ejemplo de suma de números
-*/
-
-const numbers = [3, 5, 1, 4, 2];
-
-const sumOfNumbers = numbers.reduce((accumulator, number) => accumulator + number, 0);
-
-/*
-  2. Reduce
-    c. Inmutabilidad, https://doesitmutate.xyz
-*/
-
-/*
-  3. Solución
-*/
-
-const array = [(x) => x * 1, (x) => x * 2, (x) => x * 3, (x) => x * 4];
-
-const result = array.reduce((accumulator, fn) => accumulator + fn(accumulator), 1);
-
-// console.log('The result is:', result);
-
-/*
-
-  | iteration  | fn             | expression I         | expression II     | accumulator |
-  |------------|----------------|----------------------|-------------------|-------------|
-  | 0          | (x) => x * 1   | 1 + (1) => 1 * 1     | 1 + 1             | 2           |
-  | 1          | (x) => x * 2   | 2 + (2) => 2 * 2     | 2 + 4             | 6           |
-  | 2          | (x) => x * 3   | 6 + (6) => 6 * 3     | 6 + 18            | 24          |
-  | 3          | (x) => x * 4   | 24 + (24) => 24 * 4  | 24 + 96           | 120         |
-
- */
