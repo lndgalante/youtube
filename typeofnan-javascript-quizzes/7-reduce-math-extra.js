bdf85361b0936aac5a752b1776b724823c74caa2
leo <lndgalante@gmail.com>
2021-01-10 15:39:42 -0300

    👌 IMPROVE: Reduce math extra content

--- typeofnan-javascript-quizzes/7-reduce-math-extra.js
+++ typeofnan-javascript-quizzes/7-reduce-math-extra.js
@@ -28,6 +28,7 @@ Conceptos a entender:
 
 Extra:
   - Escribiendo nuestro propio reduce
+  - Resolvamos 6 ejercicios usando reduce https://coursework.vschool.io/array-reduce-exercises
 
 Challenge + Solución:
   - https://quiz.typeofnan.dev/reduce-math
@@ -56,3 +57,121 @@ function reduce(array, reducer, initialValue) {
 
   return accumulator;
 }
+
+/*
+  - Resolvamos 6 ejercicios usando reduce
+*/
+
+/*
+  1) Convertir un array de números en un cadena de caracteres de todos esos números
+*/
+
+function stringConcat(arr) {
+  return arr.reduce((accumulator, number) => `${accumulator}${number}`, '');
+}
+
+console.log(stringConcat([1, 2, 3])); // "123"
+
+/*
+  2) Convertir un array de votantes en el total de cuantas personas votaron
+*/
+
+function totalVotes(arr) {
+  return arr.reduce((accumulator, voter) => (voter.voted === true ? accumulator + 1 : accumulator), 0);
+}
+
+const voters = [
+  { name: 'Bob', age: 30, voted: true },
+  { name: 'Jake', age: 32, voted: true },
+  { name: 'Kate', age: 25, voted: false },
+  { name: 'Sam', age: 20, voted: false },
+  { name: 'Phil', age: 21, voted: true },
+  { name: 'Ed', age: 55, voted: true },
+  { name: 'Tami', age: 54, voted: true },
+  { name: 'Mary', age: 31, voted: false },
+  { name: 'Becky', age: 43, voted: false },
+  { name: 'Joey', age: 41, voted: true },
+  { name: 'Jeff', age: 30, voted: true },
+  { name: 'Zack', age: 19, voted: false },
+];
+
+console.log(totalVotes(voters)); // 7
+
+/*
+  3) Dado un array de articulos de tu lista de deseos, obtener cuanto costaría si comprarías todo a la vez.
+      Usemos la Intl.NumberFormat API para formatear el número en la moneda que querramos.
+*/
+
+function shoppingSpree(arr) {
+  // your code here
+}
+
+var wishlist = [
+  { title: 'Tesla Model S', price: 90000 },
+  { title: '4 carat diamond ring', price: 45000 },
+  { title: 'Fancy hacky Sack', price: 5 },
+  { title: 'Gold fidgit spinner', price: 2000 },
+  { title: 'A second Tesla Model S', price: 90000 },
+];
+
+console.log(shoppingSpree(wishlist)); // 227005
+
+/*
+  4) Dado un array de arrays, aplanarlos para convertirlo en un solo array
+*/
+
+function flatten(arr) {
+  return arr.reduce((accumulator, array) => [...accumulator, ...array], []);
+}
+
+const arrays = [['1', '2', '3'], [true], [4, 5, 6]];
+
+console.log(flatten(arrays)); // ["1", "2", "3", true, 4, 5, 6];
+
+/*
+  5) Given an array of potential voters, return an object representing the results of the vote
+    Dado un array de potencial votantes, devolver el resultado de los votos
+
+    Incluyendo cuantos votantes estaban en la edad:
+     - entre 18 y 25
+     - entre 26 y 35
+     - entre 36 y 55
+     - y cuantos de ellos actualmente votaron, debemos devolver un objeto como el siguiente:
+
+      {
+        numYoungVotes: 1,
+        numYoungPeople: 4,
+        numMidVotesPeople: 3,
+        numMidsPeople: 4,
+        numOldVotesPeople: 3,
+        numOldsPeople: 4
+      }
+
+*/
+
+const voters2 = [
+  { name: 'Bob', age: 30, voted: true },
+  { name: 'Jake', age: 32, voted: true },
+  { name: 'Kate', age: 25, voted: false },
+  { name: 'Sam', age: 20, voted: false },
+  { name: 'Phil', age: 21, voted: true },
+  { name: 'Ed', age: 55, voted: true },
+  { name: 'Tami', age: 54, voted: true },
+  { name: 'Mary', age: 31, voted: false },
+  { name: 'Becky', age: 43, voted: false },
+  { name: 'Joey', age: 41, voted: true },
+  { name: 'Jeff', age: 30, voted: true },
+  { name: 'Zack', age: 19, voted: false },
+];
+
+function voterResults(arr) {
+  // your code here
+}
+
+console.log(voterResults(voters2)); // Returned value shown below:
+
+/*
+  Extra, para practicar!
+  Usar la API de fetch o librería que creas conveniente y obtener la cantidad total de estrellas que tienen tus repositorios.
+  Usando la GitHub API usando https://api.github.com/users/<YOUR GITHUB USERNAME HERE>/repos
+*/
