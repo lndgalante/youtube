ca8040e9638eb3a764bbb197003c59482a9a05e5
leo <lndgalante@gmail.com>
2021-01-25 10:54:44 -0300

    👌 IMPROVE: Short circuit content

--- typeofnan-javascript-quizzes/8-short-circuit-notification.js
+++ typeofnan-javascript-quizzes/8-short-circuit-notification.js
@@ -22,15 +22,13 @@ Conceptos a entender:
       - undefined
       - false
 
-  2. Conjunción, tabla de verdad https://excalidraw.com/#json=5524765540352000,bs0gr5NJ-e_HBOXVxVLrBQ
+  2. Conjunción y Corto-circuito, tabla de verdad https://excalidraw.com/#json=5524765540352000,bs0gr5NJ-e_HBOXVxVLrBQ
       a. Ejemplo de && como operador lógico, chequear rango de edad
+      b. Ejemplo de && como operador de control de flujo, permisos a entrada con 2 condiciones
 
-  3. Corto-circuito, tabla de verdad https://excalidraw.com/#json=5524765540352000,bs0gr5NJ-e_HBOXVxVLrBQ
-      a. Ejemplo de && como operador de control de flujo, permisos a entrada con 2 condiciones
+  3. Visualización
 
-  4. Visualización
-
-  5. Solución
+  4. Solución
 
 ------------------------------------------------------------
 
@@ -75,27 +73,24 @@ console.log('undefined', Boolean(undefined));
 console.log('false', false);
 
 /*
-2. Conjunción, tabla de verdad https://excalidraw.com/#json=5524765540352000,bs0gr5NJ-e_HBOXVxVLrBQ
-    a. Ejemplo de && como operador lógico, chequear rango de edad
-*/
-
-/*
-3. Corto-circuito, tabla de verdad https://excalidraw.com/#json=5524765540352000,bs0gr5NJ-e_HBOXVxVLrBQ
+2. Conjunción y Corto-circuito, tabla de verdad https://excalidraw.com/#json=5524765540352000,bs0gr5NJ-e_HBOXVxVLrBQ
     En resumen si A es verdadero, entonces el operador && devuelve B, de caso contrario devuelve A.
     Sabemos que los valores que A puede tomar para ser verdadero son todos los que no estan incluidos en los falsy values vistos.
     Lo importante a conocer del corto-circuito es que si el valor de A es falso entonces B no se va a evaluar.
 
-    a. Ejemplo de && como operador de control de flujo, permisos a entrada con 2 condiciones, y por último devolver ticket
+    a. Ejemplo de && como operador lógico, chequear rango de edad
+    b. Ejemplo de && como operador de control de flujo, permisos a entrada con 2 condiciones, y por último devolver ticket
+
 */
 
 /*
-4. Visualización
-  Testiemos los valores verdaderos
+3. Visualización
+    Testiemos los valores verdaderos
 */
 
 /*
-5. Solución
-  Hagamos un pequeño refactor al código.
-  Pensemos que nos devuelve la expresión "hasManyNotifications && 's'".
-  Usemos el visualizador para ver que nos devuelve la expresión anterior.
+4. Solución
+    Hagamos un pequeño refactor al código.
+    Pensemos que nos devuelve la expresión "hasManyNotifications && 's'".
+    Usemos el visualizador para ver que nos devuelve la expresión anterior.
 */
