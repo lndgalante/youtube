07dc5da90d6e5bdc2de18e641038013e25a6c1bb
leo <lndgalante@gmail.com>
2021-03-25 21:41:28 -0300

    📦 NEW: Add IIFE or HOF

---
+++ typeofnan-javascript-quizzes/12-iife-hof-or-both.js
@@ -0,0 +1,57 @@
+/*
+
+Challenge:
+  IIFE, HOF, or Both
+
+
+Descripción:
+  El siguiente snippet ilustra una Función-Inmediatamente-Invocada (IIFE),
+  una Función de Alto Orden (HOF), ambas o ninguna?
+
+Posibles opciones:
+  - Solo IIFE
+  - Solo HOF
+  - Ambas: IIFE y HOF
+  - Ninguna
+
+------------------------------------------------------------
+
+Conceptos a entender:
+  1. IIFE: Inmediately Invoked Function Expression
+
+  2. HOF: Higher Order Function
+
+  3. Solución
+
+------------------------------------------------------------
+
+Challenge + Solución:
+  - https://quiz.typeofnan.dev/iffe-hof-both
+  - https://github.com/lndgalante/youtube/blob/master/typeofnan-javascript-quizzes/12-iife-hof-or-both.js
+
+Links Principales:
+  - https://developer.mozilla.org/en-US/docs/Glossary/IIFE
+  - https://eloquentjavascript.net/05_higher_order.html
+
+Links Alternativos:
+  - https://javascript.info/var#iife
+  - https://flaviocopes.com/javascript-iife
+  - https://programmingwithmosh.com/javascript/what-are-higher-order-functions-in-javascript
+
+*/
+
+/*
+  1. IIFE: Inmediately Invoked Function Expression
+*/
+
+/*
+  2. HOF: Higher Order Function
+*/
+
+/*
+  3. Solución
+*/
+
+((fn, val) => {
+  return fn(val);
+})(console.log, 5);
