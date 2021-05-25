98fa5355f9338bc7858374f8e2c9a2b744299a59
Leonardo Galante <lndgalante@gmail.com>
2021-05-24 22:20:53 -0300

    📦 NEW: Add string methods

---
+++ typeofnan-javascript-quizzes/35-string-methods.js
@@ -0,0 +1,52 @@
+/*
+
+Challenge:
+  String Methods
+
+Descripción:
+  Los métodos de los strings son usados para trabajar con cadenas en JavaScript.
+  Qué metodo podría usarse para encontrar el valor especificado y devolver la posición del matcheo?
+
+  Por ejemplo, qué metodo te dice que "bird" esta en la posición 4 en el siguiente string llamado "word"?
+
+Posibles opciones:
+  - length
+  - indexOf
+  - find
+  - none
+
+------------------------------------------------------------
+
+Conceptos a entender:
+  1. Cuál de los siguientes métodos pertenece a los strings?
+    a. length
+    b. indexOf
+    c. find
+
+  2. Cómo funciona el método indexOf()?
+
+  3. Solución
+
+------------------------------------------------------------
+
+Challenge + Solución:
+  - https://quiz.typeofnan.dev/string-methods/
+  - https://github.com/lndgalante/youtube/blob/master/typeofnan-javascript-quizzes/35-string-methods.js
+
+Links Principales:
+  - https://javascript.info/string
+  - https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/String
+
+*/
+
+/*
+3. Solución
+
+Posibles opciones:
+  - length
+  - indexOf
+  - find
+  - none
+*/
+
+const word = 'The bird is the word';
