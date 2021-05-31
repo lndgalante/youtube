8ff61f1bbd3206359fc8f641cee1f3f330695488
Leonardo Galante <lndgalante@gmail.com>
2021-05-31 09:20:28 -0300

    📦 NEW: Finish string methods content

--- typeofnan-javascript-quizzes/35-string-methods.js
+++ typeofnan-javascript-quizzes/35-string-methods.js
@@ -23,11 +23,11 @@ Conceptos a entender:
     b. indexOf
     c. find
 
-  2. Cómo funciona el método indexOf()?
+  2. Método indexOf()
     a. Definición y ejemplo
-    b. Propiedades y características
-    c. Diagrama https://whimsical.com/youtube-UHP1pRc5k3txvDHNdcHLvp@2Ux7TurymR3srym4tVsk
-    d. Ejemplos del diagrama
+    b. Diagrama https://whimsical.com/youtube-UHP1pRc5k3txvDHNdcHLvp@2Ux7TurymR3srym4tVsk
+    c. Ejemplos del diagrama
+    d. Propiedades y características
 
   3. Solución
 
@@ -42,8 +42,8 @@ Challenge + Solución:
   - https://github.com/lndgalante/youtube/blob/master/typeofnan-javascript-quizzes/35-string-methods.js
 
 Links Principales:
-  - https://javascript.info/string
-  - https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/String
+  - https://javascript.info/string#searching-for-a-substring
+  - https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/String/indexOf
 
 */
 
@@ -59,11 +59,12 @@ La propiedad length y el método indexOf pertenecen a los strings, en cambio el 
 */
 
 /*
-  2. Cómo funciona el método indexOf()?
+  2. Método indexOf()
     a. Definición y ejemplo
 */
 
 /*
+
 - El método indexOf nos devuelve el índice de la primer ocurrencia de un valor especificado,
   si el valor no es encontrado entonces este método va a devolver -1.
 
@@ -73,18 +74,42 @@ const title = 'The Lord of the Rings: The Return of the King';
 
 const titleIndexes = title.split('').map((character) => ({ character }));
 
-console.table(titleIndexes);
+// console.table(titleIndexes);
 
-console.log(title.indexOf('Rings'));
+// console.log(title.indexOf('Hobbit'));
 
 /*
-  2. Cómo funciona el método indexOf()?
-    b. Propiedades y características
+  2. Método indexOf()
+    b. Diagrama https://whimsical.com/youtube-UHP1pRc5k3txvDHNdcHLvp@2Ux7TurymR3srym4tVsk
+*/
+
+/*
+  2. Método indexOf()
+    c. Ejemplos
+*/
+
+const phrase = 'The quick brown fox jumps';
+
+const phraseIndexes = phrase.split('').map((character) => ({ character }));
+
+// console.table(phraseIndexes);
+
+// console.log(`Cuál es el índice de "T"? ${phrase.indexOf('T')}`);
+
+// console.log(`Cuál es el índice de "The"? ${phrase.indexOf('The')}`);
+
+// console.log(`Cuál es el índice de "quick"? ${phrase.indexOf('quick')}`);
+
+// console.log(`Cuál es el índice de "fox"? ${phrase.indexOf('fox')}`);
+
+/*
+  2. Método indexOf()
+    d. Propiedades y características
 */
 
 /*
 
-  Propiedades:
+  Propiedades y características:
   - Case-sensitive
 
   - Se puede usar para chequear si un substring existe en el string
@@ -93,48 +118,30 @@ console.log(title.indexOf('Rings'));
   - Se le puede proporcionar un segundo parámetro, siendo este un indice,
     para que busque a partir del indice indicado.
 
-  - Existe lastIndexOf() que en vez de devolver la primer ocurrencia, nos devuelve la última ocurrencia
+  - Existe lastIndexOf() que en vez de devolver la primer ocurrencia,
+    nos devuelve la última ocurrencia del substring que estemos buscando
 
   - Existe el mismo método en los arrays, para buscar el índice de un elemento
+
 */
 
 /*
-  2. Cómo funciona el método indexOf()?
-    b. Diagrama https://whimsical.com/youtube-UHP1pRc5k3txvDHNdcHLvp@2Ux7TurymR3srym4tVsk
-*/
+  3. Solución
 
-/*
-  2. Cómo funciona el método indexOf()?
-    c. Ejemplos
-*/
+  Qué método podría usarse para encontrar el valor especificado y devolver la posición del matcheo?
 
-const phrase = 'The quick brown fox jumps';
+  Por ejemplo, qué metodo te dice que "bird" esta en la posición 4 en el siguiente string llamado "word"?
 
-const phraseIndexes = phrase.split('').map((character) => ({ character }));
-
-console.table(phraseIndexes);
-
-console.log(`Cuál es el índice de "T"? ${phrase.indexOf('T')}`);
-
-console.log(`Cuál es el índice de "The"? ${phrase.indexOf('The')}`);
-
-console.log(`Cuál es el índice de "quick"? ${phrase.indexOf('quick')}`);
-
-console.log(`Cuál es el índice de "fox"? ${phrase.indexOf('fox')}`);
-
-/*
-3. Solución
-
-Posibles opciones:
-  - length X
-  - indexOf O
-  - find X
-  - none X
+  Posibles opciones:
+    - length X
+    - indexOf O
+    - find X
+    - none X
 */
 
 const word = 'The bird is the word';
 
-console.log(`What's the index for "bird"? ${word.indexOf('bird')}`);
+// console.log(`Cuál es el indice de "bird"? ${word.indexOf('bird')}`);
 
 /*
   4. Extra
@@ -154,20 +161,31 @@ function indexOf(string, substring, isCaseInsensitive = false) {
   const parsedSubstring = isCaseInsensitive ? substring.toLowerCase() : substring;
   const word = allWords.find(({ word }) => word === parsedSubstring);
 
-  return word.index ?? -1;
+  return word?.index ?? -1;
 }
 
-console.log(indexOf('The quick', 'quick'));
-console.log(indexOf('The quick', 'QUIck', true));
+// console.log(indexOf('The quick', 'quick'));
+// console.log(indexOf('The quick', 'Quick', true));
 
 /*
   4. Extra
     b. Creemos nuestro método includes() usando indexOf() nativo
 */
 
-function includes(string, substring) {
+function includesNative(string, substring) {
   const index = string.indexOf(substring);
   const isFound = index !== -1;
 
   return isFound;
 }
+
+function includes(string, substring, isCaseInsensitive = false) {
+  const index = indexOf(string, substring, isCaseInsensitive);
+  const isFound = index !== -1;
+
+  return isFound;
+}
+
+console.log(includesNative('The quick', 'quick'));
+console.log(includes('The quick', 'Quick'));
+console.log(includes('The quick', 'Quick', true));
