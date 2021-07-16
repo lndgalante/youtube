ad199bfc9a890bc2ea9ecbacbfca012f1c626157
Leonardo Galante <lndgalante@gmail.com>
2021-07-16 18:06:04 -0300

    📦 NEW: Add basic template literals

---
+++ typeofnan-javascript-quizzes/55-template-literals.js
@@ -0,0 +1,40 @@
+/*
+
+Challenge:
+  Template Literals
+
+Descripción:
+  Cuáles de las versiones de JavaScript soporta la sintáxis de template literals, como la de abajo:
+
+Posibles opciones:
+  - ecmascript
+  - ES2015
+  - stage-6
+
+------------------------------------------------------------
+
+Conceptos a entender:
+  1. Qué son los template literals?
+    a. Definición
+    b. Visualización -> https://poet.krasimir.vercel.app/e/1Zaa680OQHA
+
+  2. Qué es ECMAScript?
+    a. Donde puedo ver que incluye cada release?
+
+  3. Solución
+
+  4. Extra
+    a. Uso en ejecución de una función
+    b. Uso en librerías como styled-components
+    c. Extensión de VSCode: Template String Converter
+
+------------------------------------------------------------
+
+Challenge + Solución:
+  - https://quiz.typeofnan.dev/template-literals
+  - https://github.com/lndgalante/youtube/blob/master/typeofnan-javascript-quizzes/55-template-literals.js
+
+Links Principales:
+  - https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Template_literals
+
+*/
